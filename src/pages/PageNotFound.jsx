0838881cb288c3238aca9ee025998d155a0d2b25
Judith Jude <ifunanyajudith09@gmail.com>
2024-12-03 00:50:53 +0100

    updated website

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 const PageNotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>not found</h1>
+    <div className="p-10 text-center">
+      <h1 className="font-medium">404 | Page Not Found</h1>
+      <br />
+      <button
+        onClick={() => navigate('/')}
+        className="bg-sky-500 p-2 rounded-md border-0 "
+      >
+        Go to homepage
+      </button>
     </div>
   );
 };
