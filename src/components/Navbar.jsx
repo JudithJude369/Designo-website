dabef923688fa8dc1da15700ceeb118599571eaf
Judith Jude <ifunanyajudith09@gmail.com>
2024-12-04 03:16:41 +0100

    updated website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,39 @@
 import { NavLink } from 'react-router-dom';
-import Logo from './Logo';
+import Logo from '@/components/Logo';
+import hamburger from '@/assets/shared/mobile/icon-hamburger.svg';
+import closeBtn from '@/assets/shared/mobile/icon-close.svg';
 
 const Navbar = () => {
   return (
-    <nav className="lg:flex justify-between px-6 py-8 lg:px-16 lg:text-2xl font-medium text-gray-600 ">
-      <Logo />
-      <ul className="lg:flex items-center lg:space-x-14">
-        <li className="hover:underline transition-all ease-in-out">
-          <NavLink to="/about">Our Company</NavLink>
-        </li>
-        <li className="hover:underline transition-all ease-in-out">
-          <NavLink to="/locations">Locations</NavLink>
-        </li>
-        <li className="hover:underline transition-all ease-in-out">
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="relative">
+      <nav className=" text-2xl lg:flex justify-between px-6 py-8 lg:px-16 lg:text-3xl font-medium text-gray-600 w-full lg:items-center">
+        <Logo />
+
+        <ul className="space-y-4 lg:flex lg:space-x-14 ">
+          <li className="lg:hover:underline transition-all ease-in-out pt-6 lg:pt-4">
+            <NavLink to="/about">Our Company</NavLink>
+          </li>
+          <li className="lg:hover:underline transition-all ease-in-out">
+            <NavLink to="/locations">Locations</NavLink>
+          </li>
+          <li className="lg:hover:underline transition-all ease-in-out">
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <img
+        src={hamburger}
+        alt="hamburger-menu"
+        className="lg:hidden absolute hamburger"
+      />
+
+      <img
+        src={closeBtn}
+        alt="close-btn"
+        className="lg:hidden absolute hamburger"
+      />
+    </div>
   );
 };
 
