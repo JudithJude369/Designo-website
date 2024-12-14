5419fe444fd8e4852b099b594cf19d7d82947443
Judith Jude <ifunanyajudith09@gmail.com>
2024-12-14 05:42:02 +0100

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Button = ({ children }) => {
+const Button = ({ to, children }) => {
   return (
-    <div>
-      <button className=" font-medium px-8 py-4 text-black rounded-lg bg-white mt-8">
+    <Link to={to}>
+      <button className=" cursor-pointer font-semibold px-8 py-4 text-black rounded-lg bg-white mt-8 hover:bg-red-300 focus:bg-red-300 hover:text-white transition-all ease-in-out">
         {children}
       </button>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Button from '@/components/Button';
 import phone from '@/assets/home/desktop/image-hero-phone.png';
+
 const Home = () => {
   return (
-    <main className="lg:px-24 py-2">
-      <section className="bg-red-400 text-white lg:px-12 text-center lg:text-left bg-img lg:py-16 px-4 py-12 lg:flex">
-        <div>
-          <h1 className="font-semibold">
+    <main className="lg:px-24 lg:py-8 py-0.5">
+      <section className="bg-red-400 text-white lg:px-16 text-center lg:text-left bg-img lg:pt-28  pt-12 flex lg:flex-row lg:justify-between lg:rounded-3xl flex-col justify-center">
+        <div className="lg:w-2/3">
+          <h1 className="font-semibold lg:font-semibold text-3xl lg:text-4xl ">
             Award-winning custom designs and digital branding solutions
           </h1>
           <h2 className="mt-8">
@@ -13,9 +14,14 @@ const Home = () => {
             fully responsive websites, app design, and engaging brand
             experiences. Find out more about our services.
           </h2>
-          <Button>Learn more</Button>
+          <Button to="/about">Learn more</Button>
         </div>
-        <img src={phone} alt="phone" className="pt-0" />
+        <div>
+          <img src={phone} alt="phone" className="top md:mr-12 md:ml-12" />
+        </div>
+      </section>
+      <section>
+        <h1>gkllljjs</h1>
       </section>
     </main>
   );
