3b176e8b021780610137b16724e472d6e89203f9
Judith Jude <ifunanyajudith09@gmail.com>
2025-01-07 06:52:03 +0100

    update

--- src/pages/GraphicDesign.jsx
+++ src/pages/GraphicDesign.jsx
@@ -1,4 +1,24 @@
-import React from 'react';
+const projects = [
+  {
+    id: 0,
+    title: 'Tim Brown',
+    description: "A book cover designed for Tim Brown's new release, 'Change'",
+    image: '/designo-website/assets/graphic-design/desktop/image-change.jpg',
+  },
+  {
+    id: 1,
+    title: 'Boxed Water',
+    description: 'A simple packaging concept made for Boxed Water',
+    image:
+      '/designo-website/assets/graphic-design/desktop/image-boxed-water.jpg',
+  },
+  {
+    id: 2,
+    title: 'Science!',
+    description: 'A poster made in collaboration with the Federal Art Project',
+    image: '/designo-website/assets/graphic-design/desktop/image-science.jpg',
+  },
+];
 
 const GraphicDesign = () => {
   return (
--- src/pages/WebDesign.jsx
+++ src/pages/WebDesign.jsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import imgExpress from '@/assets/web-design/desktop/image-express.jpg';
+import imgTransfer from '@/assets/web-design/desktop/image-transfer.jpg';
+import imgPhonton from '@/assets/web-design/desktop/image-photon.jpg';
+import imgBuilder from '@/assets/web-design/desktop/image-builder.jpg';
+import imgBlogr from '@/assets/web-design/desktop/image-blogr.jpg';
+import imgCamp from '@/assets/web-design/desktop/image-camp.jpg';
 
 const WebDesign = () => {
   return (
@@ -11,6 +16,46 @@ const WebDesign = () => {
           <p className="mt-4 lg:w-1/2 md:w-3/4 mx-auto px-4 lg:px-0">
             We build websites that serve as powerful marketing tools and bring
             memorable brand experiences.
+          </p>
+        </div>
+      </section>
+
+      <section className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div>
+          <img src={imgExpress} alt="" />
+          <h3>Express</h3>
+          <p>A multi-carrier shipping website for ecommerce businesses</p>
+        </div>
+        <div>
+          <img src={imgTransfer} alt="" />
+          <h3>Transfer</h3>
+          <p>
+            Site for low-cost money transfers and sending money within seconds
+          </p>
+        </div>
+        <div>
+          <img src={imgPhonton} alt="" />
+          <h3>Photon</h3>
+          <p>
+            A state-of-the-art music player with high-resolution audio and DSP
+            effects
+          </p>
+        </div>
+        <div>
+          <img src={imgBuilder} alt="" />
+          <h3>Builder</h3>
+          <p>Connects users with local contractors based on their location</p>
+        </div>
+        <div>
+          <img src={imgBlogr} alt="" />
+          <h3>Blogr</h3>
+          <p>Blogr is a platform for creating an online blog or publication</p>
+        </div>
+        <div>
+          <img src={imgCamp} alt="" />
+          <h3>Camp</h3>
+          <p>
+            Get expert training in coding, data, design, and digital marketing
           </p>
         </div>
       </section>
