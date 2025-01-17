668f845a686299a3de4d9c2346672819114e07d2
Judith Jude <ifunanyajudith09@gmail.com>
2025-01-17 18:29:25 +0100

    main

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,11 +1,12 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import { Outlet } from 'react-router-dom';
-
+import ScrollToTop from '@/components/ScrollToTop';
 const AppLayout = () => {
   return (
     <main>
       <Navbar />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </main>
